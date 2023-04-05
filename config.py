931634f62429328d91e68093c66ee8c059cfbc26
Rishu <67968330+itz-rishu@users.noreply.github.com>
2023-04-05 15:49:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʏᴀ {first}👋!\n\n✭ ᴡʜᴀᴛ ɪ ᴅᴏ?\n\n\n\n‣ ɪ ᴄᴀɴ sᴛᴏʀᴇ ʏᴏᴜʀ ғɪʟᴇs/ᴛᴇxᴛs.\n\n‣ ᴛʜɪs ʙᴏᴛ ᴅᴇsɪɢɴ sᴛᴏʀᴇ ᴍᴇᴅɪᴀ/ғɪʟᴇs. ᴅᴜᴇ ᴛᴏ ᴍᴜᴄʜ ʟᴏᴀᴅ ʙᴏᴛ ᴄᴀɴ ʙᴇ ʀᴇsᴘᴏɴsᴇ sʟᴏᴡʟʏ (ᴅᴇᴘᴇɴᴅs ᴏɴ ʟᴏᴀᴅ).\n\n‣ ᴛʜɪs sᴘᴇᴄɪᴀʟʟʏ ғᴏʀ ᴡʜᴏ ᴡᴀɴᴛ ᴛᴏ sᴛᴏʀᴇ ғɪʟᴇs\n\n‣ **ᴊᴜsᴛ sᴇɴᴅ ᴍᴇ ʏᴏᴜʀ ғɪʟᴇ/ᴛᴇxᴛ/ᴍᴇᴅɪᴀ/ & ɢᴇᴛ ʏᴏᴜʀ sʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ.**")
+START_MSG = os.environ.get("START_MESSAGE", "")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "**ᴍᴜsᴛ ᴊᴏɪɴ ᴛʜɪs ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɢʀᴏᴜᴘ ᴛᴏ ᴜsᴇ ᴍᴇ ᴄʟɪᴄᴋ /start ᴀɢᴀɪɴ ᴀғᴛᴇʀ ᴊᴏɪɴ**")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>ᴍᴜsᴛ ᴊᴏɪɴ ᴛʜɪs ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɢʀᴏᴜᴘ ᴛᴏ ᴜsᴇ ᴍᴇ ᴄʟɪᴄᴋ /start ᴀɢᴀɪɴ ᴀғᴛᴇʀ ᴊᴏɪɴ</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
