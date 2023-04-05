6715f813915a79f23174a9eca44591f20da9de0f
Rishu <67968330+itz-rishu@users.noreply.github.com>
2023-04-05 15:40:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>➻ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>˹ʀɪsʜᴜ˼🥀</a>\n➻ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n➻ Library : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ {__version__}</a>\n➻ ᴄʜᴀɴɴᴇʟ : @DeltaApkMods\n➻ ᴄʜᴀᴛ ɢʀᴏᴜᴘ : @BESTIES_ZONE</b>",
+            text = f"<b>➻ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>˹ʀɪsʜᴜ˼🥀</a>\n➻ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n➻ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ {__version__}</a>\n➻ ᴄʜᴀɴɴᴇʟ : @DeltaApkMods\n➻ ᴄʜᴀᴛ ɢʀᴏᴜᴘ : @BESTIES_ZONE</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
