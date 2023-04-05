ac2a9580077cb974b2f984f99c8cc6f56bea47ce
Rishu <67968330+itz-rishu@users.noreply.github.com>
2023-04-05 19:44:44 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 sʜᴀʀᴇ ᴜʀʟ", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ</b>\n\n{link}\n\n<b>ᴍᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ <a href="https://t.me/BT46ER">˹ʀɪsʜᴜ˼🥀</a></b>", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ</b>\n\n{link}\n\n<b>ᴍᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ <a href='https://t.me/BT46ER'>˹ʀɪsʜᴜ˼🥀</a></b>", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -57,4 +57,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 sʜᴀʀᴇ ᴜʀʟ", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ</b>\n\n{link}\n\n<b>ᴍᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ <a href="https://t.me/BT46ER">˹ʀɪsʜᴜ˼🥀</a></b>", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ</b>\n\n{link}\n\n<b>ᴍᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ <a href='https://t.me/BT46ER'>˹ʀɪsʜᴜ˼🥀</a></b>", quote=True, reply_markup=reply_markup)
